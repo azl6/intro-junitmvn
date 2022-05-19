be322778beacf5f5e6220fb182715f68706b52aa
Alex <alex.rodrigues23997@gmail.com>
2022-05-19 11:01:33 -0300

    JUnit5 Test lifecycle demonstration

--- src/test/java/com/azold6/introjunit/GreetingTest.java
+++ src/test/java/com/azold6/introjunit/GreetingTest.java
@@ -1,24 +1,45 @@
 package com.azold6.introjunit;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
-    Greeting greeting;
+
+    private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("@BeforeAll, only called once");
+    }
 
     @BeforeEach
-    void setUp(){
-        System.out.println("In @BeforeEach...");
+    void setUp() {
+        System.out.println("@BeforeEach");
         greeting = new Greeting();
     }
 
     @Test
     void helloWorld() {
         System.out.println(greeting.helloWorld());
+
     }
 
     @Test
-    void testHelloWorld() {
-        System.out.println(greeting.helloWorld("Alex"));
+    void helloWorld1() {
+        System.out.println(greeting.helloWorld("John"));
+    }
+
+    @Test
+    void helloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("@AfterEach");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("@AfterAll, only called once");
     }
 }
