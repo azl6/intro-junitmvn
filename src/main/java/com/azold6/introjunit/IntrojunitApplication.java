950896c57a73acea55897de7212f2134076ba81c
Alex <alex.rodrigues23997@gmail.com>
2022-05-19 09:30:30 -0300

    Initial commit

---
+++ src/main/java/com/azold6/introjunit/IntrojunitApplication.java
@@ -0,0 +1,13 @@
+package com.azold6.introjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrojunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrojunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azold6/introjunit/IntrojunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azold6.introjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrojunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
