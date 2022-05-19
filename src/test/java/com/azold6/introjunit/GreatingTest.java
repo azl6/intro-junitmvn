120a111b2eebf6e0ba6ac20923bf844499369813
Alex <alex.rodrigues23997@gmail.com>
2022-05-19 10:48:02 -0300

    Spring starter dependencies and base tests created

---
+++ src/test/java/com/azold6/introjunit/GreatingTest.java
@@ -0,0 +1,16 @@
+package com.azold6.introjunit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreatingTest {
+
+    @Test
+    void helloWorld() {
+    }
+
+    @Test
+    void testHelloWorld() {
+    }
+}
